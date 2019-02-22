a29293530801697117eb4dc00c56011951a60f1f
Eric LaBouve <elabouve@csc235x02.ad.calpoly.edu>
2019-02-22 09:26:02 -0800

    Initial Commit

---
+++ csc436lab7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  csc436lab7
+//
+//  Created by Eric LaBouve on 2/22/19.
+//  Copyright © 2019 elabouve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ csc436lab7Tests/csc436lab7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  csc436lab7Tests.swift
+//  csc436lab7Tests
+//
+//  Created by Eric LaBouve on 2/22/19.
+//  Copyright © 2019 elabouve. All rights reserved.
+//
+
+import XCTest
+@testable import csc436lab7
+
+class csc436lab7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
